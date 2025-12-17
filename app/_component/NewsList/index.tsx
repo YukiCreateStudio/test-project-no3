71d17c2fda9f72014b266f17da507a63ecba7e6a
YukiCreateStudio <yuki.createstudio@gmail.com>
2025-12-18 00:01:18 +0900

    P76まで完成

---
+++ app/_component/NewsList/index.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import styles from "./index.module.css";
+import Image from "next/image";
+import { News } from "@/app/_libs/microcms";
+import CategoryTag from "../CategoryTag";
+import DateTag from "../DateTag";
+
+type Props = {
+  news: News[];
+};
+
+export default function NewsList({ news }: Props) {
+  return (
+    <>
+      <ul>
+        {news.map((article) => (
+          <li key={article.id} className={styles.list}>
+            <Link href={`/news`} className={styles.link}>
+              <Image
+                className={styles.image}
+                src="/no-image.png"
+                alt="no-image"
+                width={1200}
+                height={630}
+              />
+              <dl>
+                <dt className={styles.title}>{article.title}</dt>
+                <dd className={styles.meta}>
+                  <CategoryTag category={article.category} />
+                  <DateTag date={article.publishedAt ?? article.createdAt} />
+                </dd>
+              </dl>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
